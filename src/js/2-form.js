e623e1e9b4a754176505a38cda1a8724c3293738
StanislavBondarenko631 <skyritm201287@gmail.com>
2026-07-10 14:09:55 +0300

    finish hw mod 9 js

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,1 +1,52 @@
-console.log('Form');
+const formData = {
+  email: '',
+  message: '',
+};
+
+const STORAGE_KEY = 'feedback-form-state';
+
+const form = document.querySelector('.feedback-form');
+const emailInput = document.querySelector('.feedback-form input[name="email"]');
+const messageInput = document.querySelector(
+  '.feedback-form textarea[name="message"]'
+);
+
+const saveData = localStorage.getItem(STORAGE_KEY);
+if (saveData) {
+  const parsedData = JSON.parse(saveData);
+
+  formData.email = parsedData.email || '';
+  formData.message = parsedData.message || '';
+
+  emailInput.value = formData.email;
+  messageInput.value = formData.message;
+}
+
+form.addEventListener('input', onFormInput);
+
+function onFormInput(event) {
+  formData[event.target.name] = event.target.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  if (formData.email.trim() === '' || formData.message.trim() === '') {
+    alert('Fill please all fields');
+    return;
+  }
+  console.log({
+    email: formData.email.trim(),
+    message: formData.message.trim(),
+  });
+
+  localStorage.removeItem(STORAGE_KEY);
+
+  form.reset();
+
+  formData.email = '';
+  formData.message = '';
+}
